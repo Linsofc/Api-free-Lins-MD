de1084b1d116aa1f89c3f845521033abe9708ca5
Linsofc <137659454+Linsofc@users.noreply.github.com>
2025-01-22 04:21:26 +0700

    Lins-MD

--- db/games/lins.js
+++ db/games/lins.js
@@ -1,1 +1,13 @@
-
+const fetch = require("node-fetch")
+async function games (type) {
+  try {
+    const response = await fetch(`https://raw.githubusercontent.com/Linsofc/Api-free-Lins-MD/main/db/games/${type}.json`);
+    if (!response.ok) {
+      throw new Error(`Failed to fetch game data: ${response.statusText}`);
+    }
+    const gameJson = await response.json();
+    return { status: true, creator: "Lins-MD", data: gameJson }
+  } catch (err) {
+   return { status: false, creator: "Lins-MD", data: { msg: "Eror: ", err }}
+  }
+}
